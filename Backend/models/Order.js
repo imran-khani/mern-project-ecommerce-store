dbdb5585eba46bf4e39a5dd7611cc433a63f65ab
Imran khan <imranloveh01@gmail.com>
2024-09-22 00:04:33 +0500

    create order model

---
+++ Backend/models/Order.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+
+const orderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    products: [
+            {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true,
+            },
+            quantity: {
+                type: Number, 
+                required: true,
+            }
+        }
+    ],
+    totalPrice: {type:Number,required:true,},
+    orderStatus:{type:String,required:true, default:'Processing'},
+    createdAt:{type:Date,default:Date.now()}
+
+}, { timestamps: true })
